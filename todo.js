dcd0ebf279ce3c989064c2bfe0aa40bf65ce1a17
Ahmed Raza <ahmedr.0331@gmail.com>
2024-12-17 12:57:34 +0500

    added delete todo feature

--- todo.js
+++ todo.js
@@ -1,4 +1,4 @@
-import {collection, addDoc, db, onSnapshot, serverTimestamp, query, orderBy,} from './firebase.js'
+import {collection, addDoc, db, onSnapshot, serverTimestamp, query, orderBy, doc, deleteDoc} from './firebase.js'
 
 let taskInput = document.getElementById('taskInput')
 let taskList = document.getElementById('taskList')
@@ -17,11 +17,21 @@ const getData = () =>{
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
         taskList.innerHTML  = ""
         querySnapshot.forEach((data) => {
-        taskList.innerHTML += `<li>${data.data().value}</li>`
+        taskList.innerHTML += `<li>
+        ${data.data().value} 
+          <span onclick="dltTodo('${data.id}')">Delete todo</span>
+     </li>`
     });
 
-  console.log("Geting data....",  taskList.value);
+  console.log("Geting data....");
 });
 }
 getData()
+
 document.getElementById('addTask').addEventListener('click', addTask)
+
+window.dltTodo = async(id) => { 
+    console.log(id, 'this is id');
+    
+    await deleteDoc(doc(db, "todos", id));
+}
